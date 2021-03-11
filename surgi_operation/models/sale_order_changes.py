37ab8b7d946be8d2b875ab10a1e62b47cedb42de
Ahmed <ahmed887463@gmail.com>
2021-03-11 08:45:46 +0200

    Update sale_order_changes.py

--- surgi_operation/models/sale_order_changes.py
+++ surgi_operation/models/sale_order_changes.py
@@ -67,10 +67,10 @@ class sale_order(models.Model):
                 'domain': [('id', 'in', list)],
                 # 'context': {"search_default_type_group":1,},
             }
-        pass
+        
     
         
-   def pruchase_interchange(self):
+    def pruchase_interchange(self):
         sourcedocument=self.id
         porder=self.env['purchase.order'].search([['auto_sale_order_id','=',sourcedocument]])
         sql='update purchase_order set picking_type_id=%d'%self.deliver_to
